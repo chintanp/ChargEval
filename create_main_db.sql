afdd3f7516e2e8c1ccbd116fa1e8e363001500e4
chintan pathak <chintan.pathak@gmail.com>
2019-10-20 15:59:43 -0700

    WIP architecture - tables

---
+++ create_main_db.sql
@@ -0,0 +1,12 @@
+-- Database: wsdot_evse_main
+
+-- DROP DATABASE wsdot_evse_main;
+
+CREATE DATABASE wsdot_evse_main
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
